6a418d85e2fb310e122a882bbae3998d67d48ba7
sik2 <devpang20@gmail.com>
2021-07-14 04:09:00 +0900

    feat:test code

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello");
+const name = "sik2",
+    jobs = "programmer",
+    gender = "male"
+
+const getInfo = (name, jobs, gender)=> {
+    console.log(`Hello ${name}, you are ${jobs}, you are a ${gender}`);
+};
+
+getInfo(name, jobs, gender);
+
+export {};
